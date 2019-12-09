d04d2f04f1437895a2cf88ad8a073b895f4ad0e7
Ken Aiken <ken@hypack.com>
2019-12-09 11:11:46 -0500

    Finished Day 3

--- Day3/day3.py
+++ Day3/day3.py
@@ -1,53 +1,93 @@
+from bisect import bisect_left 
 
-class pt2d:
-    def __init__( self, x, y ):
-        self.x = x
-        self.y = y
-    def __cmp__ ( self, p ):
-        bool = self.x==p.x and self.y==p.y
-        print( "cmp is " + str(bool) )
-    def toString( self ):
-        return "(" + str(self.x) + "," + str(self.y) + ")"
+class pt3d:
+	def __init__( self, x, y ):
+		self.x = x
+		self.y = y
+		self.z = 0
+	def __init__( self, x, y, z ):
+		self.x = x
+		self.y = y
+		self.z = z
+	def __eq__ ( self, p ):
+		return self.x==p.x and self.y==p.y
+	def __ne__ ( self, p ):
+		return not (self==p)
+	def distance( self):
+		return abs(self.x)+abs(self.y*1.00001)
+	def __lt__ ( self, p ):
+		return self.distance()<p.distance()
+	def __gt__ ( self, p ):
+		return self.distance()>p.distance()
+	def __le__ ( self, p ):
+		return self==p or self.distance()<p.distance()
+	def __ge__ ( self, p ):
+		return self==p or self.distance()>p.distance()
+	def __str__( self ):
+		return "(" + str(self.x) + "," + str(self.y) + ")"
+	def copy( self ):
+		return pt3d(self.x,self.y,self.z)
+	def getTransit(self):
+		return self.z
+
+def BinarySearch(a, x): 
+    i = bisect_left(a, x) 
+    if i != len(a) and a[i] == x: 
+        return i 
+    else: 
+        return -1
 
 traversed = []
 inters = []
+def day3a():
+	inters.sort(key=pt3d.distance)  
+	f = open("inters.txt", "w")
+	for p in inters:
+		 f.write( str(p)+", dist "+ str(p.distance()) +"\n" )
+	f.close() 
+def day3b():
+	inters.sort(key=pt3d.getTransit)  
+	f = open("inters.txt", "w")
+	for p in inters:
+		 f.write( str(p)+", Transit "+ str(p.getTransit()) +"\n" )
+	f.close() 
 
 f = open("input.txt", "r")
-sum = 0;
+line = 0
 for x in f:
-    lastPt = pt2d( 0, 0 )    
-    cmds = x.split(',')
-    print("NEW LINE")
-    for c in cmds:
-        if( len(c) < 2 ):
-            continue
-        print(c)
-        dir = c[0]
-        cnt = int(c[1:len(c)])
-        list = ""
-        for i in range(cnt):
-            if(dir == 'R') :
-                lastPt.x += 1
-            elif(dir == 'L') :
-                lastPt.x -= 1
-            elif(dir == 'U') :
-                lastPt.y += 1
-            elif(dir == 'D') :
-                lastPt.y -= 1
-            list = list + lastPt.toString()
-            has = 0
-            for p in traversed :
-                if( p == lastPt):
-                    has = 1
-                    break;            
-            print( "Count for " + lastPt.toString() +" : ", traversed.count(lastPt))
-            if( has < 1):
-                traversed.append(lastPt)
-                print( lastPt.toString() )
-            else:
-                inters.append(lastPt)
-        print( list )
-                
-f = open("inters.txt", "w")
-for p in inters:
-    f.write( p.toString() )
+	line+=1
+	transit = 0
+	traversed.sort(key=pt3d.distance)
+	lastPt = pt3d( 0, 0, 0 )    
+	cmds = x.split(',')
+	print("NEW LINE")
+	for c in cmds:
+		if( len(c) < 2 ):
+			continue
+		print(c)
+		dir = c[0]
+		cnt = int(c[1:len(c)])
+		list = ""
+		for i in range(cnt):
+			if(dir == 'R') :
+				lastPt.x += 1
+			elif(dir == 'L') :
+				lastPt.x -= 1
+			elif(dir == 'U') :
+				lastPt.y += 1
+			elif(dir == 'D') :
+				lastPt.y -= 1
+			lastPt.z += 1
+			list = list + str(lastPt)
+			has = 0
+			if(line>1):
+				has = BinarySearch(traversed,lastPt)
+				if( has >= 0):
+					it = lastPt.copy()
+					it.z += traversed[has].z
+					inters.append(it)
+					print( str(lastPt) )
+			else:
+				traversed.append(lastPt.copy())
+
+day3b()
